998e1724f6926f6bd082034edebcb3c7533c6f70
serheihimself <sergm080@gmail.com>
2023-10-14 16:21:08 +0300

    pagination final commit

--- js/swipper.js
+++ js/swipper.js
@@ -1,7 +1,6 @@
 new Swiper(".slider_rewievs",{
     pagination: {
         el: ".swiper-pagination",
-        type: 'custom', // Используем пользовательскую пагинацию
         clickable: !0,
         renderBullet: function(e, n) {
             return '<span class="' + n + '">0' + (e + 1) + "</span>"

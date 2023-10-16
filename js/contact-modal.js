f9e787fadf4fd8381b53cec23bf48dfd40ccd2ea
serheihimself <sergm080@gmail.com>
2023-10-16 20:32:15 +0300

    contact modal commit

---
+++ js/contact-modal.js
@@ -0,0 +1,35 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[contact-modal-open]"),
+    closeModalBtn: document.querySelector("[contact-modal-close]"),
+    modal: document.querySelector("[contact-modal]"),
+    backdrop: document.querySelector(".backdrop"),
+  };
+
+  refs.openModalBtn.addEventListener("click", openModal);
+  refs.closeModalBtn.addEventListener("click", closeModal);
+  refs.backdrop.addEventListener("click", onBackdropClick);
+  window.addEventListener("keydown", onKeyPress);
+
+  function openModal() {
+    refs.modal.classList.remove("is-hidden");
+    document.body.classList.add("scroll-lock");
+  }
+
+  function closeModal() {
+    refs.modal.classList.add("is-hidden");
+    document.body.classList.remove("scroll-lock");
+  }
+
+  function onKeyPress(event) {
+    if (event.code === "Escape") {
+      closeModal();
+    }
+  }
+
+  function onBackdropClick(event) {
+    if (event.target === refs.backdrop) {
+      closeModal();
+    }
+  }
+})();
